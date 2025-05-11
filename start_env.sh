08cd22f9821a8680c7d2f7d87b766eb299a59dfa
Louis Gutierrez <louisgutierrez@MacBook-Pro-3.local>
2025-05-11 11:22:48 -0500

    slim down Dockerfile and use compose

--- start_env.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-docker container run \
-  --rm \
-  --name dev_env \
-  -p 10000:8888 \
-  --volume /Users/louisgutierrez/Documents/xai_dev_env:/home/jovyan/work \
-  dev_env \
-  jupyter lab --NotebookApp.token=''
